4006a4f84eee5615537ad2477569cf771602c868
Matthias Dunkel <matthias.dunkel@web.de>
2023-03-23 16:25:38 +0100

    feat: implement first version of a jsenv

---
+++ examples/reactive/jsenv.js
@@ -0,0 +1,24 @@
+// Namespacing
+class JSEnv {
+    constructor(){
+        this.eventQueue = []
+    }
+
+    pollEvents(){
+        var events = this.eventQueue;
+        this.eventQueue = []
+        return events
+    }
+
+    waitForSecs(ms) {
+        var eventId = "waitForSecs" + Date.now() + ms
+        
+        setTimeout(() => {
+            this.eventQueue = this.eventQueue.concat( {id: eventId, data: undefined, type: "waitForSecs"} )
+        }, ms)
+
+        return eventId
+    }
+}
+
+
