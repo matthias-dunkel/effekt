ab6010a992167cea40f03fbc9200738809792597
Matthias Dunkel <matthias.dunkel@web.de>
2023-04-06 12:40:33 +0200

    feat: input event

--- examples/reactive/jsenv.js
+++ examples/reactive/jsenv.js
@@ -18,6 +18,21 @@ class JSEnv {
 
         return eventId
     }
+
+    question(text) {
+        var readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+
+        var eventId = "question" + Date.now() + text
+        readline.question(text + "\n", answer => {
+            this.eventQueue = this.eventQueue.concat({id: eventId, data: answer, type: "question"})
+            readline.close()
+        })
+
+        return eventId
+    }
 }
 
 
