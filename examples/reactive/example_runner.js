07b6b966669394ab307dac526e23dbd22239eaaa
Matthias Dunkel <matthias.dunkel@web.de>
2023-04-06 12:56:45 +0200

    feat: Add simple Example Runner

---
+++ examples/reactive/example_runner.js
@@ -0,0 +1,6 @@
+// Before running this file you need to compile the exmaple.effekt file.
+// Compile from inside the examples folder with: effekt --compile reactive/examples.effekt
+// This is only needed for input related exmaples.
+
+var app = require('../out/reactive_examples.js')
+app.main().run()
