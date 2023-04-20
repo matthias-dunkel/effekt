8b69d266c8e4c7cdb9c2615d73bb8c84282ec22f
Matthias Dunkel <matthias.dunkel@web.de>
2023-04-20 16:49:49 +0200

    feat: move examples into test cases

---
+++ examples/reactive/tests/jsenv/guessTheNumber_runner.js
@@ -0,0 +1,6 @@
+// Before running this file you need to compile the exmaple.effekt file.
+// Compile from inside the examples folder with: effekt --compile reactive/tests/jsenv/guessTheNumber.effekt
+// This is only needed for input related exmaples.
+
+var app = require('../../../out/reactive_guessTheNumber.js')
+app.main().run()
